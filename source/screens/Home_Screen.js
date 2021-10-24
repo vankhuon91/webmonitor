438b32d924078c91c1aacbde710c9a3d3e7b6ba7
Invisible16 <julia.nguyen0616@gmail.com>
2021-10-24 16:51:35 +0700

    start project

---
+++ source/screens/Home_Screen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Home_Screen = () => {
+  return (
+    <View>
+      <Text>this is home screen</Text>
+    </View>
+  );
+};
+
+export default Home_Screen;
+
+const styles = StyleSheet.create({});
